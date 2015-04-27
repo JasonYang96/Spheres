0bda99127f51e1c5dbb9501c2535ae6ce325ae93
Jason Yang <JasonYang96@ucla.edu>
2015-04-27 03:08:15 -0700

    working solar system with some basic lighting

--- spheres.js
+++ spheres.js
@@ -7,6 +7,7 @@ var colorIndex = 0;
 
 //perspective matrix variables
 var pMatrix;
+var pMatrixLoc;
 var fovx = 45.0;
 var aspect;
 var near = 0.1;
@@ -21,8 +22,14 @@ var Matrix;
 var MatrixLoc;
 
 //light variables
-var lightAmbient = vec4(1.0, 1.0, 0.0, 1.0);
-var ambientColor, diffuseColor, specularClor;
+var lightAmbient = vec4(0.5, 0.5, 0.5, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 0.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var ambientProductLoc, diffuseProductLoc, specularProductLoc;
+var ambientProduct, diffuseProduct, specularProduct;
+var lightPositionLoc;
+var shininessLoc;
+var sunMatrixLoc;
 
 //x,y,z coord, and heading variable for camera movement
 var coord = [ 0, Math.sin(Math.PI/6) * -75, -75 ];
@@ -43,14 +50,14 @@ var vd = vec4(0.816497, -0.471405, 0.333333,1);
 var planets = [];
 
 //creates a planet
-function planet(x,n,s,t,dt)
-{
+function planet(x,n,s,t,dt, material, shading) {
     this.translateMatrix = translate(x, 0, 0);
     this.sMatrix = scale(s, s, s);
     this.theta = t;
     this.dtheta = dt;
     this.starting = index;
-
+    this.material = material;
+    this.shading = shading;
     //create a sphere
     tetrahedron(va, vb, vc, vd, n);
     this.numPoints = index - this.starting;
@@ -60,12 +67,15 @@ function triangle(a, b, c) {
      pointsArray.push(a);
      pointsArray.push(b);      
      pointsArray.push(c);
+     var u = subtract(b, a);
+     var v = subtract(c, a);
+     var normal = vec4(normalize(cross(v,u)), true);
+     normal[3] = 0.0;
     
      // normals are vectors
-     
-     normalsArray.push(a[0],a[1], a[2], 0.0);
-     normalsArray.push(b[0],b[1], b[2], 0.0);
-     normalsArray.push(c[0],c[1], c[2], 0.0);
+     normalsArray.push(normal);
+     normalsArray.push(normal);
+     normalsArray.push(normal);
 
      index += 3;  
 }
@@ -108,11 +118,48 @@ window.onload = function init()
     if ( !gl ) { alert( "WebG isn't available" ); }
 
     //creating planets and moon
-    planets.push(new planet(7, 3, .8, 0, 2));
-    planets.push(new planet(15, 3, .5, 90, 1.5));
-    planets.push(new planet(20, 3, .7, 270, 1));
-    planets.push(new planet(25, 3, .9, 299, 1));
-    planets.push(new planet(10, 3, .2, 180, .5));
+    planets.push(new planet(0, 5, 3, 0, 0, {
+        ambient: vec4(1.0, 1.0, 0.0, 1.0),
+        diffuse: vec4(1.0, 1.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 0.0, 1.0),
+        shininess: 0.0,
+        shading: 0,
+    }));
+    planets.push(new planet(7, 5, .8, 0, 2, {
+        ambient: vec4(1.0, 0.0, 0.0, 1.0),
+        diffuse: vec4(1.0, 0.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 1.0, 1.0),
+        shininess: 5.0,
+        shading: 0,
+    }));
+    planets.push(new planet(15, 5, .5, 90, 1.5, {
+        ambient: vec4(1.0, 0.0, 0.0, 1.0),
+        diffuse: vec4(1.0, 0.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 1.0, 1.0),
+        shininess: 10.0,
+        shading: 0,
+    }));
+    planets.push(new planet(20, 5, .7, 270, 1, {
+        ambient: vec4(1.0, 0.0, 0.0, 1.0),
+        diffuse: vec4(1.0, 0.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 1.0, 1.0),
+        shininess: 20.0,
+        shading: 0,
+    }));
+    planets.push(new planet(25, 5, .9, 299, 1, {
+        ambient: vec4(1.0, 0.0, 0.0, 1.0),
+        diffuse: vec4(1.0, 0.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 1.0, 1.0),
+        shininess: 30.0,
+        shading: 0,
+    }));
+    planets.push(new planet(10, 5, .2, 180, 2, {
+        ambient: vec4(0.0, 1.0, 0.0, 1.0),
+        diffuse: vec4(0.0, 1.0, 0.0, 1.0),
+        specular: vec4(1.0, 1.0, 1.0, 1.0),
+        shininess: 30.0,
+        shading: 0,
+    }));
 
     //configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -124,8 +171,13 @@ window.onload = function init()
     gl.useProgram( program );
 
     // get variables from shader
-    vColorLoc = gl.getUniformLocation( program, "vColor" );
     MatrixLoc = gl.getUniformLocation( program, "Matrix");
+    ambientProductLoc = gl.getUniformLocation( program, "ambientProduct");
+    diffuseProductLoc = gl.getUniformLocation( program, "diffuseProduct");
+    specularProductLoc = gl.getUniformLocation( program, "specularProduct");
+    pMatrixLoc = gl.getUniformLocation( program, "pMatrix");
+    shininessLoc = gl.getUniformLocation( program, "shininess");
+    sunMatrixLoc = gl.getUniformLocation( program, "sunMatrix");
 
     //create and bind buffer for vertices
     var vBuffer = gl.createBuffer();
@@ -136,6 +188,15 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
+
+    //create and bind buffer for normals
+    var nBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW );
+
+    var vNormal = gl.getAttribLocation( program, "vNormal");
+    gl.vertexAttribPointer( vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray( vNormal );
 
     //event listener
     window.onkeydown = function(event) {
@@ -192,8 +253,7 @@ window.onload = function init()
 };
 
 //returns fovy based on fovx
-function fovy()
-{
+function fovy() {
     return ( 2 * Math.atan(Math.tan(radians(fovx)/2) / aspect) * 180 / Math.PI);
 }
 
@@ -201,43 +261,44 @@ function render() {
     //clear canvas
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
-    //apply model-view matrix
-    pMatrix = mult(mult(perspective(fovy(), aspect, near, far), rotate( 30, [1,0,0])), rotate(headingAngle, [ 0, 1, 0 ]));
-    tMatrix = mult(pMatrix, translate( coord[0], coord[1], coord[2]));
-    scaleMatrix = mult(tMatrix, scale(3,3,3));
+    //calculate prospective Matrix
+    pMatrix = perspective(fovy(), aspect, near, far);
+    gl.uniformMatrix4fv(pMatrixLoc, false, flatten(pMatrix));
 
-    //create stationary sun
-    gl.uniformMatrix4fv(MatrixLoc, false, flatten(scaleMatrix));
-    gl.uniform4fv(vColorLoc, [1.0, 1.0, 0.0, 1.0]);
-    for (var j = 0; j < index; j+= 3) {
-        gl.drawArrays(gl.TRIANGLES, j , index);
-    }
+    tMatrix = mult(rotate( 30 + headingAngle, [1,0,0]), translate( coord[0], coord[1], coord[2]));
 
     //instance the spheres
     for (var i = 0; i < planets.length; i++) {
+        planets[i].theta += planets[i].dtheta;
+        
+        //calculate light variables
+        ambientProduct = mult(lightAmbient, planets[i].material.ambient);
+        diffuseProduct = mult(lightDiffuse, planets[i].material.diffuse);
+        specularProduct = mult(lightSpecular, planets[i].material.specular);
+        gl.uniform4fv(ambientProductLoc, ambientProduct);
+        gl.uniform4fv(diffuseProductLoc, diffuseProduct);
+        gl.uniform4fv(specularProductLoc, specularProduct);
+        gl.uniform1f(shininessLoc, planets[i].material.shininess);
+        
         //create the moon on the last planet drawn
         if (i == planets.length - 1)
         {
-            planets[i].theta += planets[i].dtheta;
             scaleMatrix = mult(Matrix, planets[i].sMatrix);
             rotMatrix = mult(rotate(planets[i].theta, [0,1,0]), planets[i].translateMatrix);
             Matrix = mult(scaleMatrix,rotMatrix);
-
-            ambientProduct = mult(lightAmbient, [0.0, 1.0, 0.0, 1.0]);
-
-            gl.uniform4fv(vColorLoc, [0.0, 1.0, 0.0, 1.0]);
         }
         //draw the other planets
         else
         {
-            planets[i].theta += planets[i].dtheta;
-            scaleMatrix = mult(tMatrix, planets[i].sMatrix);
-            rotMatrix = mult(scaleMatrix, rotate(planets[i].theta, [0, 1, 0]));
-            Matrix = mult(rotMatrix, planets[i].translateMatrix);
-
-            gl.uniform4fv(vColorLoc, [ 1.0, 0.0, 0.0, 1.0 ]);
+            Matrix = mult(tMatrix, planets[i].sMatrix);
+            Matrix = mult(Matrix, rotate(planets[i].theta, [0, 1, 0]));
+            Matrix = mult(Matrix, planets[i].translateMatrix);
         }
 
+        if (i == 0)
+        {
+            gl.uniformMatrix4fv(sunMatrixLoc, false, flatten(Matrix));
+        }
         gl.uniformMatrix4fv(MatrixLoc, false, flatten(Matrix));
         gl.drawArrays(gl.TRIANGLES, planets[i].starting , planets[i].numPoints);
     }
